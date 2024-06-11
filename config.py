11acf38b1e7d4e50a79a7c301f58cf371f281e89
Crime2321 <305396877@qq.com>
2024-06-11 23:58:59 +0800

    upload tag mapping prompt and code

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
+# path
 data_path = '../data/weibo_user_data'
 neo4j_path = '../data/neo4j_data'
 graph_path = '../data/graph_data'
 result_path = '../data/result_data'
+vector_db_path = '../data/vector_db'
+
+# retrieve top k results
+top_k = 10
+
+# openai api key, api base and model name
+api_key = "sk-b0fpqBSg3rctiLCiEG9umm9zhwPHkjWuKYUuTYEINYAxSihv"
+api_base = "https://api.chatanywhere.com.cn/v1"
+model = "gpt-3.5-turbo-0613"
