6aad8fdf4cd3b9c433c46a5b1404d0e33a48ff81
Crime2321 <305396877@qq.com>
2024-06-21 17:21:41 +0800

    增加标签处理代码及prompt

--- config.py
+++ config.py
@@ -12,3 +12,8 @@ top_k = 10
 api_key = "sk-b0fpqBSg3rctiLCiEG9umm9zhwPHkjWuKYUuTYEINYAxSihv"
 api_base = "https://api.chatanywhere.com.cn/v1"
 model = "gpt-3.5-turbo-0613"
+
+# ali cloud api
+api_key_ali = 'sk-7c87c08117ec4226ba8ffb82a5f841b0'
+api_base_ali = 'https://dashscope.aliyuncs.com/compatible-mode/v1'
+model_ali = 'qwen-plus'
